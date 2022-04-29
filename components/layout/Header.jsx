78f93980f2be821f1971d1a8d44aae80f7358287
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-29 16:45:19 -0500

    Add Header component
    This will be displayed across all pages

---
+++ components/layout/Header.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className='flex justify-between items-center bg-[#373b53]'>
+      <div className='bg-one p-6 leading-zero  rounded-r-3xl'>
+        <Image src='/assets/logo.svg' alt='main logo' width={28} height={26} />
+      </div>
+      <div className=' flex items-center leading-zero'>
+        <div className='pr-6'>
+          <Image
+            src='/assets/icon-moon.svg'
+            alt='theme switcher'
+            width={20}
+            height={20}
+          />
+        </div>
+        <div className='px-6 py-5 border-l border-[#494e6e]'>
+          <div className='rounded-full overflow-hidden'>
+            <Image
+              src='/assets/image-avatar.jpg'
+              alt='user avatar'
+              width={32}
+              height={32}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
