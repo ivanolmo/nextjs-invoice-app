8ecba2327a8962e9f9ffbbab187e172da3f0413c
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-29 19:49:39 -0500

    Add Invoice404 component that renders when no invoices found
    Name pending :)

---
+++ components/layout/Invoice404.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export default function Invoice404() {
+  return (
+    <div className='mt-24 flex flex-col items-center'>
+      <Image
+        src='/assets/illustration-empty.svg'
+        alt='no invoices found'
+        width={193}
+        height={160}
+      />
+      <div className='mt-10'>
+        <h2 className='text-xl font-bold tracking-tighter text-center'>
+          There is nothing here
+        </h2>
+        <p className='text-xs text-six tracking-tight text-center mt-5 w-40'>
+          Create an invoice by clicking the{' '}
+          <span className='font-bold'>New</span> button and get started
+        </p>
+      </div>
+    </div>
+  );
+}
