788c8c58a16b317836c4716e51cd78abe4fb3e90
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-29 23:37:49 -0500

    Refactor to improve styling and Tailwind conditional classes

--- components/invoice/InvoiceListItem.jsx
+++ components/invoice/InvoiceListItem.jsx
@@ -1,10 +1,15 @@
-import { formatDate, formatMoney, formatStatus } from '../../lib/formatUtils';
+import {
+  formatDate,
+  formatMoney,
+  formatStatus,
+  classNames,
+} from '../../lib/formatUtils';
 
 export default function InvoiceListItem({ invoice }) {
   const { id, paymentDue, total, clientName, status } = invoice;
 
   return (
-    <section className='flex justify-between p-6 bg-white rounded-lg text-sm'>
+    <li className='flex justify-between my-4 p-6 bg-white rounded-lg text-sm'>
       <div className='flex flex-col gap-6'>
         <div className='font-bold'>
           <span className='text-seven'>#</span>
@@ -17,20 +22,22 @@ export default function InvoiceListItem({ invoice }) {
           </div>
         </div>
       </div>
-      <div className='flex flex-col items-end gap-6'>
+      <div className='flex flex-col items-end gap-6 w-[6.5rem]'>
         <div className='text-seven tracking-tight'>{clientName}</div>
         <div
-          className={
+          className={classNames(
             status === 'paid'
-              ? 'text-green bg-green/10 py-2 px-8 font-bold rounded-sm'
+              ? 'text-green bg-green/10'
               : status === 'pending'
-              ? 'text-orange bg-orange/10 py-2 px-8 font-bold rounded-sm'
-              : 'text-darkGray bg-darkGray/10 py-2 px-8 font-bold rounded-sm'
-          }
+              ? 'text-orange bg-orange/10'
+              : 'text-darkGray bg-darkGray/10',
+            'p-2.5 font-bold rounded-md w-full text-center'
+          )}
         >
+          <span className='bg-current inline-block mr-2 w-2 h-2 rounded-full'></span>
           {formatStatus(status)}
         </div>
       </div>
-    </section>
+    </li>
   );
 }
