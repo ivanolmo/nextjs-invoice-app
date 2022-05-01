c952d6d5660d3e041fd05996a98827051d931a87
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-30 22:54:31 -0500

    Refactor /invoices page
    Add getStaticProps to pull invoice data from Firebase with ISR set to 12 hours
    Invoice data is stored in state and passed where it is utilized
    Invoice count no longer simulated and the invoice state is checked for length
    Invoice404 component shows if invoice data is empty or null

--- pages/invoices/index.jsx
+++ pages/invoices/index.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import InvoiceList from '../../components/invoice/InvoiceList';
 import UtilityHeader from '../../components/layout/UtilityHeader';
 import Invoice404 from '../../components/layout/Invoice404';
 
-export default function Invoices() {
-  // simulate invoice count from state
-  const invoiceCount = 7;
+export default function Invoices({ invoices }) {
+  const [invoiceData, setInvoiceData] = useState([]);
+
+  useEffect(() => {
+    setInvoiceData(invoices);
+  }, [invoices]);
+
+  if (!invoiceData || invoiceData.length === 0) {
+    return <Invoice404 />;
+  }
 
   return (
     <main>
-      <UtilityHeader />
-      {invoiceCount === 0 ? <Invoice404 /> : <InvoiceList />}
+      <UtilityHeader invoiceCount={invoiceData.length} />
+      <InvoiceList invoices={invoices} />
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://invoice-app-e8de3-default-rtdb.firebaseio.com/invoices.json'
+  );
+  const invoices = await res.json();
+
+  return {
+    props: { invoices },
+    revalidate: 43200,
+  };
 }
