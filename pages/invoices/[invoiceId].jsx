459bfb75d30e0037e209bd18246b6c4f7a1458ca
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-01 22:39:14 -0500

    Add dynamic invoice page
    This page displays components for a single invoice

---
+++ pages/invoices/[invoiceId].jsx
@@ -0,0 +1,35 @@
+import Invoice from '../../components/invoice/Invoice';
+import BackButton from '../../components/ui/BackButton';
+import { getInvoiceById, getInvoices } from '../../lib/dbUtils';
+
+export default function InvoicePage({ invoice }) {
+  return (
+    <main>
+      <section className='pt-8 pb-14 px-6'>
+        <BackButton />
+        <Invoice invoice={invoice} />
+      </section>
+      {/* TODO edit/delete/mark buttons */}
+    </main>
+  );
+}
+
+export async function getStaticProps(context) {
+  const invoiceId = context.params.invoiceId;
+  const invoice = await getInvoiceById(invoiceId);
+
+  return {
+    props: {
+      invoice,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const invoices = await getInvoices();
+
+  return {
+    paths: invoices.map((invoice) => ({ params: { invoiceId: invoice.id } })),
+    fallback: false,
+  };
+}
