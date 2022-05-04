edfb6fa65cfa1289e45be22184b4748c448e1e91
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-04 16:32:43 -0500

    Add back button component

---
+++ components/ui/BackButton.jsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+export default function BackButton() {
+  const router = useRouter();
+
+  return (
+    <button className='ml-6' type='button' onClick={() => router.back()}>
+      <Image
+        src='/assets/icon-arrow-left.svg'
+        alt='left arrow'
+        width={6}
+        height={8}
+      />
+      <span className='text-xs tracking-tight font-bold ml-6'>Go Back</span>
+    </button>
+  );
+}
