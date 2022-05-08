08cfbf52d2be6619a09e40ccb9dfcbaf756b1bac
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-08 01:02:07 -0500

    Add component that renders new invoice actions
    Contains button components for Discard, Save as Draft, and Save & Send

---
+++ components/invoice/new-invoice-comps/NewInvoiceActions.jsx
@@ -0,0 +1,17 @@
+import { useFormikContext } from 'formik';
+
+import DiscardBtn from '../../ui/DiscardBtn';
+import DraftBtn from '../../ui/DraftBtn';
+import SaveSendBtn from '../../ui/SaveSendBtn';
+
+export default function NewInvoiceActions() {
+  const { submitForm } = useFormikContext();
+
+  return (
+    <div className='flex justify-center items-center gap-2 bg-white py-5 -mx-6 mt-20 shadow-[0_-30px_30px_12px_rgba(0,0,0,0.2)]'>
+      <DiscardBtn />
+      <DraftBtn />
+      <SaveSendBtn onSubmit={submitForm} />
+    </div>
+  );
+}
