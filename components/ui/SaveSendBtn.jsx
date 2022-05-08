ce3400a2901fe08ff58c48c197af92321a04641c
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-08 01:02:59 -0500

    Add Formik onSubmit prop to submit button

--- components/ui/SaveSendBtn.jsx
+++ components/ui/SaveSendBtn.jsx
@@ -1,11 +1,11 @@
-export default function SaveSendBtn({ onClick }) {
+export default function SaveSendBtn({ onSubmit }) {
   return (
     <div
-      onClick={onClick}
-      className='flex justify-center items-center bg-one hover:bg-two h-12 px-4 rounded-full cursor-pointer'
+      onClick={() => onSubmit()}
+      className='flex justify-center items-center flex-shrink-0 bg-one hover:bg-two h-12 px-4 rounded-full cursor-pointer'
     >
       <div className='flex justify-center items-center text-white text-xs font-bold tracking-tight'>
-        {'Save & Send'}
+        Save & Send
       </div>
     </div>
   );
