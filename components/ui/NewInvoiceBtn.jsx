e2fa7bddc517c70e92a5fe938a862632ec0e6205
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-09 23:17:29 -0500

    Refactor to show NewInvoice form component when clicked

--- components/ui/NewInvoiceBtn.jsx
+++ components/ui/NewInvoiceBtn.jsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 
-export default function NewInvoiceBtn() {
+export default function NewInvoiceBtn({ setShowInvoiceForm }) {
   return (
-    <button className='bg-one hover:two text-eleven flex items-center rounded-full'>
+    <div
+      onClick={() => setShowInvoiceForm(true)}
+      className='bg-one hover:bg-two text-eleven flex items-center rounded-full cursor-pointer'
+    >
       <div className='flex ml-1.5 -mr-2 p-3 bg-[white] rounded-full'>
         <Image
           src='/assets/icon-plus.svg'
@@ -12,7 +15,7 @@ export default function NewInvoiceBtn() {
         />
       </div>
       {/* check for window size and change button text on tablet size and up */}
-      <p className='my-3 mx-4 text-sm font-bold leading-tight'>New</p>
-    </button>
+      <span className='my-3 mx-4 text-sm font-bold leading-tight'>New</span>
+    </div>
   );
 }
