a26a494a364fda584cbaf489a1f43a7c236ab64a
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-11 16:11:20 -0500

    Main _app wrapped in context provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,13 @@
+import { InvoiceProvider } from '../store/context';
 import MainHeader from '../components/layout/MainHeader';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <InvoiceProvider>
       <MainHeader />
       <Component {...pageProps} />
-    </>
+    </InvoiceProvider>
   );
 }
 
