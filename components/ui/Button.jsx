e1be9484e3383b850fb0ad3a48951e2350356d5e
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-12 09:30:12 -0500

    Add new reusable Button component
    This component will probably be the singular button component after refactoring is done

---
+++ components/ui/Button.jsx
@@ -0,0 +1,22 @@
+import { classNames } from '../../lib/formatUtils';
+
+export default function Button(props) {
+  return (
+    <div
+      onClick={() => props.onClick?.()}
+      className={classNames(
+        props.containerClasses,
+        'flex justify-center items-center h-12 px-4 rounded-full cursor-pointer'
+      )}
+    >
+      <div
+        className={classNames(
+          props.textClasses,
+          'flex justify-center items-center text-xs font-bold tracking-tight'
+        )}
+      >
+        {props.buttonText}
+      </div>
+    </div>
+  );
+}
