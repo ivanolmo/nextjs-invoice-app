8fae73c875d7afe6c2b0b5ee70da75f55633038e
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-18 22:32:39 -0500

    Refactor data fetch
    Implemented useSWR and mutate to refresh invoice view after a data change
    Implemented useState and useEffect to check for updated invoices and store them on page mount

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 import InvoiceList from '../components/invoice/InvoiceList';
 import UtilityHeader from '../components/layout/UtilityHeader';
 import Invoice404 from '../components/layout/Invoice404';
 import NewInvoice from '../components/invoice/NewInvoice';
 import InvoiceContext from '../store/context';
-import { getAllInvoices } from '../lib/db';
+import { getAllInvoices } from '../lib/dbAdmin';
+import fetcher from '../utils/fetcher';
 
-export default function Home({ invoices }) {
+export default function Home({ invoiceData }) {
   const { showInvoiceForm } = useContext(InvoiceContext);
+
+  const [invoices, setInvoices] = useState(invoiceData);
+
+  const { data, mutate } = useSWR('/api/invoice/', fetcher, {
+    revalidateOnMount: true,
+  });
+
+  useEffect(() => {
+    if (data) {
+      const invoiceArr = [];
+
+      for (const key in data) {
+        invoiceArr.push(...data[key]);
+      }
+
+      setInvoices(invoiceArr);
+    }
+  }, [data]);
 
   if (!invoices || invoices.length === 0) {
     return <Invoice404 />;
   }
+
+  mutate();
 
   return (
     <main>
@@ -25,10 +47,10 @@ export default function Home({ invoices }) {
 }
 
 export async function getStaticProps() {
-  const invoices = await getAllInvoices();
+  const invoiceData = await getAllInvoices();
 
   return {
-    props: { invoices },
-    revalidate: 43200,
+    props: { invoiceData },
+    revalidate: 10,
   };
 }
