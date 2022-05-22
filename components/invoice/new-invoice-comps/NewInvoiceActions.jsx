c54c8556c2f05924b05e0cf3c9a6f80f1138018a
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-21 20:03:39 -0500

    Refactor button behavior
    Draft button now submits form with draft values
    Now takes draft submit fn via props

--- components/invoice/new-invoice-comps/NewInvoiceActions.jsx
+++ components/invoice/new-invoice-comps/NewInvoiceActions.jsx
@@ -4,9 +4,9 @@ import { useFormikContext } from 'formik';
 import Button from '../../ui/Button';
 import InvoiceContext from '../../../store/context';
 
-export default function NewInvoiceActions() {
+export default function NewInvoiceActions({ onSubmitDraftInvoice }) {
   const { setShowInvoiceForm } = useContext(InvoiceContext);
-  const { submitForm } = useFormikContext();
+  const { values, submitForm } = useFormikContext();
 
   return (
     <div className='flex justify-center items-center gap-2 bg-white py-5 -mx-6 mt-20 shadow-[0_-30px_30px_12px_rgba(0,0,0,0.1)]'>
@@ -20,13 +20,13 @@ export default function NewInvoiceActions() {
         containerClasses='bg-[#373b53] hover:bg-eight px-4'
         textClasses='text-six'
         buttonText='Save as Draft'
-        onClick={() => {}}
+        onClick={() => onSubmitDraftInvoice(values)}
       />
       <Button
         containerClasses='bg-one hover:bg-two px-4'
         textClasses='text-white'
         buttonText='Save & Send'
-        onClick={submitForm}
+        onClick={() => submitForm()}
       />
     </div>
   );
