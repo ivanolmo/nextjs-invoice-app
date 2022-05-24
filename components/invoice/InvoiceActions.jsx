2f73df4a0add10d5eb589a164ef9bdbc5d3fa14f
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-24 17:52:29 -0500

    Refactor buttons
    Edit button now displays invoice form populated with current invoice values
    The Mark as Paid/Paid button only appears if invoice is not draft

--- components/invoice/InvoiceActions.jsx
+++ components/invoice/InvoiceActions.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+
 import Button from '../ui/Button';
 import { classNames } from '../../lib/formatUtils';
+import InvoiceContext from '../../store/context';
 
 export default function InvoiceActions({ status }) {
   // TODO display buttons conditionally on invoice
+  const { setShowInvoiceForm, currentInvoice } = useContext(InvoiceContext);
+
   return (
     <footer className='flex justify-center items-center gap-2 bg-white mt-14 px-6 py-5'>
       <Button
@@ -12,7 +17,9 @@ export default function InvoiceActions({ status }) {
         )}
         textClasses='text-seven'
         buttonText='Edit'
-        onClick={() => {}}
+        onClick={() => {
+          setShowInvoiceForm(true);
+        }}
       />
       <Button
         containerClasses={classNames(
@@ -21,21 +28,20 @@ export default function InvoiceActions({ status }) {
         )}
         textClasses='text-white'
         buttonText='Delete'
-        onClick={() => {}}
+        onClick={() => {}} //TODO implement
       />
-      <Button
-        containerClasses={
-          status === 'paid'
-            ? 'bg-green px-6 justify-self-end'
-            : 'bg-one hover:bg-two px-6'
-        }
-        textClasses='text-white'
-        buttonText={status === 'paid' ? 'Invoice Paid' : 'Mark as Paid'}
-        onClick={status === 'paid' ? () => {} : () => {}}
-        disabled={status === 'paid'}
-      />
-
-      {/* {status !== 'draft' && <InvoiceStatusBtn status={status} />} */}
+      {currentInvoice.status !== 'draft' && (
+        <Button
+          containerClasses={
+            status === 'paid'
+              ? 'bg-green px-6 justify-self-end cursor-not-allowed'
+              : 'bg-one hover:bg-two px-6'
+          }
+          textClasses='text-white'
+          buttonText={status === 'paid' ? 'Invoice Paid' : 'Mark as Paid'}
+          onClick={status === 'paid' ? () => {} : () => {}} //TODO implement
+        />
+      )}
     </footer>
   );
 }
