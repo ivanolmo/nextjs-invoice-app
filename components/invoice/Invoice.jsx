a44adb50acc647ca508dd2ca37d4e1f486520838
Ivan Olmo <ivan.olmo@gmail.com>
2022-05-28 00:37:52 -0500

    Refactor logic and styles
    Incorporated all buttons into Invoice component and removed InvoiceActions
    Updated some styling, mainly spacing
    Added 'No Items' to items box if no items
    Delete/Mark As Paid buttons still need to be implemented

--- components/invoice/Invoice.jsx
+++ components/invoice/Invoice.jsx
@@ -5,9 +5,8 @@ import { nanoid } from 'nanoid';
 
 import InvoiceContext from '../../store/context';
 import InvoiceHeader from './InvoiceHeader';
-import InvoiceActions from './InvoiceActions';
-import InvoiceForm from './InvoiceForm';
-import { formatDate, formatMoney } from '../../lib/formatUtils';
+import Button from '../ui/Button';
+import { formatDate, formatMoney, classNames } from '../../lib/formatUtils';
 
 export default function Invoice({ invoice }) {
   const {
@@ -24,18 +23,24 @@ export default function Invoice({ invoice }) {
     total,
   } = invoice;
 
-  const { setCurrentInvoice, showInvoiceForm } = useContext(InvoiceContext);
+  const { setCurrentInvoice, setShowEditInvoiceForm } =
+    useContext(InvoiceContext);
+
+  const router = useRouter();
+
+  const handleClose = () => {
+    setCurrentInvoice(null);
+    router.push('/');
+  };
 
   useEffect(() => {
     setCurrentInvoice(invoice);
   }, [setCurrentInvoice, invoice]);
 
-  const router = useRouter();
-
   return (
     <>
-      <div className={showInvoiceForm ? 'invisible' : ''}>
-        <div className='ml-6 cursor-pointer' onClick={() => router.push('/')}>
+      <div className='pt-8'>
+        <div className='ml-6 cursor-pointer' onClick={() => handleClose()}>
           <Image
             src='/assets/icon-arrow-left.svg'
             alt='left arrow'
@@ -45,65 +50,75 @@ export default function Invoice({ invoice }) {
           <span className='text-xs tracking-tight font-bold ml-6'>Go Back</span>
         </div>
         <InvoiceHeader status={status} />
-        <section className='bg-white mt-4 mx-6 p-6 text-sm tracking-tight rounded-md'>
-          <div className='font-bold'>
-            <span className='text-seven'>#</span>
-            {id}
+        <div className='bg-white mt-4 mx-6 p-6 text-sm tracking-tight rounded-md'>
+          <div className='space-y-1'>
+            <div className='font-bold text-xs'>
+              <span className='text-seven'>#</span>
+              {id}
+            </div>
+            <div className='text-seven text-xs'>{description || 'N/A'}</div>
           </div>
-          <div className='text-seven'>{description}</div>
-          <div className='text-seven mt-7'>
-            <p>{senderAddress.street}</p>
-            <p>{senderAddress.city}</p>
-            <p>{senderAddress.postCode}</p>
-            <p>{senderAddress.country}</p>
+          <div className='text-xs text-seven mt-7 space-y-1'>
+            <p>{senderAddress.street || '-'}</p>
+            <p>{senderAddress.city || '-'}</p>
+            <p>{senderAddress.postCode || '-'}</p>
+            <p>{senderAddress.country || '-'}</p>
           </div>
           <div className='flex gap-10 mt-7'>
             <div className='flex flex-col justify-between'>
-              <div>
+              <div className='space-y-2'>
                 <h3 className='text-seven'>Invoice Date</h3>
-                <p className='text-base font-bold mt-2'>
+                <p className='text-base font-bold'>
                   {createdAt ? formatDate(createdAt) : '-'}
                 </p>
               </div>
-              <div>
+              <div className='space-y-2'>
                 <h3 className='text-seven'>Payment Due</h3>
-                <p className='text-base font-bold mt-2'>
-                  {paymentDue ? formatDate(paymentDue) : '-'}
+                <p className='text-base font-bold'>
+                  {paymentDue ? formatDate(paymentDue) : 'N/A'}
                 </p>
               </div>
             </div>
-            <div className=''>
+            <div className='space-y-2'>
               <h3 className='text-seven'>Bill To</h3>
-              <div className='mt-2'>
-                <p className='text-base font-bold mt-2'>{clientName}</p>
-                <p>{clientAddress.street}</p>
-                <p>{clientAddress.city}</p>
-                <p>{clientAddress.postCode}</p>
-                <p>{clientAddress.country}</p>
+              <div className='space-y-2'>
+                <p className='text-base font-bold mt-2'>
+                  {clientName || 'N/A'}
+                </p>
+                <div className='text-xs text-seven space-y-1'>
+                  <p>{clientAddress.street || '-'}</p>
+                  <p>{clientAddress.city || '-'}</p>
+                  <p>{clientAddress.postCode || '-'}</p>
+                  <p>{clientAddress.country || '-'}</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className='mt-4'>
-            <h3 className='text-seven'>Sent To</h3>
-            <p className='text-base font-bold'>{clientEmail}</p>
+          <div className='mt-9 space-y-3'>
+            <h3 className='text-seven text-xs'>Sent To</h3>
+            <p className='text-base font-bold'>{clientEmail || 'N/A'}</p>
           </div>
           <div className='bg-eleven mt-10 p-6 space-y-6 rounded-t-md'>
-            {items.map((item) => (
-              <div
-                key={nanoid(6)}
-                className='flex justify-between items-center'
-              >
-                <div>
-                  <h3 className='font-bold'>{item.name}</h3>
-                  <p className='text-seven font-bold mt-2'>
-                    {item.quantity} x {formatMoney(item.price)}
-                  </p>
+            {!items.length ? (
+              <div className='text-base font-bold'>No Items</div>
+            ) : (
+              items.map((item) => (
+                <div
+                  key={nanoid(6)}
+                  className='flex justify-between items-center'
+                >
+                  <div>
+                    <h3 className='font-bold'>{item.name || 'N/A'}</h3>
+                    <p className='text-seven font-bold mt-2'>
+                      {item.quantity} x {formatMoney(item.price)}
+                    </p>
+                  </div>
+                  <div>
+                    <p className='font-bold'>{formatMoney(item.total)}</p>
+                  </div>
                 </div>
-                <div>
-                  <p className='font-bold'>{formatMoney(item.total)}</p>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
           <div className='flex justify-between items-center text-white bg-[#373b53] p-6 rounded-b-md'>
             <h3>Grand Total</h3>
@@ -111,10 +126,42 @@ export default function Invoice({ invoice }) {
               {formatMoney(total)}
             </p>
           </div>
-        </section>
-        <InvoiceActions status={status} />
+        </div>
+        <div className='flex justify-center items-center gap-2 bg-white mt-14 -mx-6 py-5'>
+          <Button
+            containerClasses={classNames(
+              status === 'paid' ? 'invisible' : '',
+              'bg-buttonLight hover:bg-five px-6'
+            )}
+            textClasses='text-seven'
+            buttonText='Edit'
+            onClick={() => {
+              setShowEditInvoiceForm(true);
+            }}
+          />
+          <Button
+            containerClasses={classNames(
+              status === 'paid' ? 'invisible' : '',
+              'bg-nine hover:bg-ten px-6'
+            )}
+            textClasses='text-white'
+            buttonText='Delete'
+            onClick={() => {}} //TODO implement
+          />
+          <Button
+            containerClasses={
+              status === 'draft'
+                ? 'invisible'
+                : status === 'paid'
+                ? 'bg-green px-6 justify-self-end cursor-not-allowed'
+                : 'bg-one hover:bg-two px-6'
+            }
+            textClasses='text-white'
+            buttonText={status === 'paid' ? 'Invoice Paid' : 'Mark as Paid'}
+            onClick={status === 'paid' ? () => {} : () => {}} //TODO implement
+          />
+        </div>
       </div>
-      {showInvoiceForm && <InvoiceForm invoice={invoice} />}
     </>
   );
 }
