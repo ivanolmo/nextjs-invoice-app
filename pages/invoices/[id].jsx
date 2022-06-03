8d3aa76e8a75058c1a7f975f719eb0e51bed4d28
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-02 21:34:31 -0500

    Refactor dynamic page to refresh data when updated
    Uses useSWR to revalidate data
    Used to render new invoice data after invoice update is submitted

--- pages/invoices/[id].jsx
+++ pages/invoices/[id].jsx
@@ -1,12 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 import InvoiceContext from '../../store/context';
 import Invoice from '../../components/invoice/Invoice';
 import InvoiceEdit from '../../components/invoice/InvoiceEdit';
 import { db } from '../../lib/firebaseAdmin';
+import fetcher from '../../utils/fetcher';
 
-export default function InvoicePage({ invoice }) {
+export default function InvoicePage({ invoiceData }) {
+  const [invoice, setInvoice] = useState(invoiceData);
+
   const { showEditInvoiceForm } = useContext(InvoiceContext);
+
+  // refresh invoice on screen if data is updated
+  const { data, mutate } = useSWR(`/api/invoice/${invoice.id}`, fetcher, {
+    revalidateOnMount: true,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setInvoice(data.invoice);
+    }
+  }, [data]);
+
+  mutate();
 
   return (
     <>
@@ -22,9 +39,9 @@ export async function getServerSideProps(context) {
 
   const doc = await db.collection('invoices').doc(id).get();
 
-  const invoice = doc.data();
+  const invoiceData = doc.data();
 
-  if (!invoice) {
+  if (!invoiceData) {
     return {
       notFound: true,
     };
@@ -32,7 +49,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      invoice,
+      invoiceData,
     },
   };
 }
--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,6 +1,5 @@
 const fetcher = async (url) => {
   const res = await fetch(url);
-  console.log('fetcher');
 
   return res.json();
 };
