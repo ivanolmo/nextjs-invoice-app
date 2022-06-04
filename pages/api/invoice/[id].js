fe71fb5a01adbff361c67cce602b6b0204708d95
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-04 12:52:19 -0500

    Refactor API routes
    Split into two routes
    /api/invoices handles getting all invoices and posting new invoices to the list
    /api/invoice/[id] handles single invoice functions
    get, delete, patch, and put are all on dynamic route

---
+++ pages/api/invoice/[id].js
@@ -0,0 +1,95 @@
+import { db } from '../../../lib/firebaseAdmin';
+import { addDays } from '../../../lib/formatUtils';
+
+export default async function handler(req, res) {
+  const {
+    query: { id },
+    body,
+    method,
+  } = req;
+
+  switch (method) {
+    // get invoice by id
+    case 'GET':
+      try {
+        const doc = await db.collection('invoices').doc(id).get();
+
+        const invoice = doc.data();
+
+        if (invoice) {
+          res.status(200).json({ invoice });
+        } else {
+          res.status(404).json({ message: 'Invoice not found' });
+        }
+      } catch (error) {
+        res.status(500).json({ message: 'Invoice lookup failure' });
+      }
+
+      break;
+
+    // delete invoice with the specified id
+    case 'DELETE':
+      try {
+        await db.collection('invoices').doc(id).delete();
+        res.status(200).json({ message: 'Invoice delete successful' });
+      } catch (error) {
+        res.status(500).json({ message: 'Invoice delete failure' });
+      }
+      break;
+
+    // update the specified invoice 'status' field from pending to paid
+    case 'PATCH':
+      try {
+        await db.collection('invoices').doc(id).update({
+          status: 'paid',
+        });
+        res.status(200).json({ message: 'Status update success' });
+      } catch (error) {
+        res.status(500).json({ message: 'Status update failure' });
+      }
+      break;
+
+    // update the entire invoice, all fields are required
+    case 'PUT':
+      try {
+        const { createdAt, paymentTerms, items } = body;
+
+        const formattedCreatedAt = new Date(createdAt).toISOString();
+
+        const paymentDue = addDays(formattedCreatedAt, +paymentTerms);
+
+        const total = items.reduce(
+          (acc, item) => acc + item.quantity * item.price,
+          0
+        );
+
+        for (let item of items) {
+          item.total = item.quantity * item.price;
+        }
+
+        const updatedInvoice = {
+          ...body,
+          status: 'pending',
+          createdAt: formattedCreatedAt,
+          paymentDue,
+          paymentTerms: +paymentTerms,
+          total,
+        };
+
+        await db
+          .collection('invoices')
+          .doc(updatedInvoice.id)
+          .set(updatedInvoice);
+
+        res.status(200).json({ updatedInvoice });
+      } catch (error) {
+        res.status(500).json({ message: 'Invoice update failure' });
+      }
+      break;
+
+    default:
+      res.setHeader('Allow', ['GET', 'PATCH', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} not allowed`);
+      break;
+  }
+}
---
+++ pages/api/invoices/index.js
@@ -0,0 +1,87 @@
+import { db } from '../../../lib/firebaseAdmin';
+import { addDays, generateId } from '../../../lib/formatUtils';
+
+export default async function handler(req, res) {
+  const { body, method } = req;
+
+  switch (method) {
+    // get all invoices
+    case 'GET':
+      try {
+        const snapshot = await db
+          .collection('invoices')
+          .orderBy('paymentDue', 'asc')
+          .get();
+
+        const invoices = [];
+        const undatedInvoices = [];
+
+        // check for invoices with no date and move them to end
+        // firebase treats null as first as far as ordering
+        snapshot.forEach((doc) => {
+          if (!doc.data().paymentDue) {
+            undatedInvoices.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          } else {
+            invoices.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          }
+        });
+
+        const allInvoicesData = invoices.concat(undatedInvoices);
+
+        res.status(200).json({ allInvoicesData });
+      } catch (error) {
+        res.status(500).json({ message: 'Invoices lookup failure' });
+      }
+      break;
+
+    // create new invoice and post to invoices list
+    case 'POST':
+      try {
+        const { createdAt, paymentTerms, items } = body;
+
+        const formattedCreatedAt =
+          createdAt === ''
+            ? new Date().toISOString()
+            : new Date(createdAt).toISOString();
+
+        const paymentDue = !paymentTerms
+          ? ''
+          : addDays(formattedCreatedAt, +paymentTerms);
+
+        const total = items.reduce(
+          (acc, item) => acc + item.quantity * item.price,
+          0
+        );
+
+        for (let item of items) {
+          item.total = item.quantity * item.price;
+        }
+
+        const newInvoice = {
+          ...body,
+          id: generateId(),
+          createdAt: formattedCreatedAt,
+          paymentDue,
+          paymentTerms: +paymentTerms,
+          total,
+        };
+
+        await db.collection('invoices').doc(newInvoice.id).set(newInvoice);
+
+        res.status(201).json({ message: 'Invoice creation success' });
+      } catch (error) {
+        res.status(500).json({ message: 'Invoice creation failure' });
+      }
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${method} not allowed`);
+      break;
+  }
+}
