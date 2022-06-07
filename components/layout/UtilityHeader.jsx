dd1c7f70271f8b7af1fdfeb680a987a99035028d
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-06 22:49:06 -0500

    Refactor NewInvoiceButton into existing Button comp
    Refactored button that was its own comp into the Button comp being used everywhere else in app
    Deleted NewInvoiceButton comp

--- components/layout/UtilityHeader.jsx
+++ components/layout/UtilityHeader.jsx
@@ -1,21 +1,42 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+
+import Button from '../ui/Button';
 import FilterButton from '../ui/FilterButton';
-import NewInvoiceBtn from '../ui/NewInvoiceBtn';
+import InvoiceContext from '../../store/context';
 
 export default function UtilityHeader({ invoiceCount }) {
+  const { setShowAddInvoiceForm } = useContext(InvoiceContext);
+
   return (
     <div className='flex justify-between items-center'>
       <div>
-        <h1 className='text-xl md:text-3xl font-bold tracking-tight'>
-          Invoices
-        </h1>
+        <h1 className='text-xl font-bold tracking-tight'>Invoices</h1>
         <h3 className='text-six text-xs tracking-tight'>
           {invoiceCount ? invoiceCount : 'No'} Invoices
         </h3>
       </div>
       <div className='flex justify-between items-center gap-4'>
         <FilterButton />
-        {/* TODO Refactor to Button comp */}
-        <NewInvoiceBtn buttonText={'New'} buttonIcon={true} />
+        <Button
+          containerClasses='bg-one hover:bg-two pr-6 pl-1.5 gap-2'
+          textClasses='text-eleven text-sm'
+          buttonText={
+            <>
+              <span className='md:hidden'>New</span>
+              <span className='hidden md:block'>New Invoice</span>
+            </>
+          }
+          icon={
+            <Image
+              src='/assets/icon-plus.svg'
+              alt='add invoice'
+              width={10}
+              height={10}
+            />
+          }
+          onClick={() => setShowAddInvoiceForm(true)}
+        />
       </div>
     </div>
   );
