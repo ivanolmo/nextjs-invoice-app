67dc3380c7f3164ed16c64186af8e89a046531a4
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-23 10:41:19 -0500

    Remove context state no longer in use

--- context/InvoiceContext.js
+++ context/InvoiceContext.js
@@ -6,13 +6,11 @@ export const InvoiceProvider = ({ children }) => {
   const [showAddInvoiceForm, setShowAddInvoiceForm] = useState(false);
   const [showEditInvoiceForm, setShowEditInvoiceForm] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [currentInvoice, setCurrentInvoice] = useState(null);
 
   const clearAll = () => {
     setShowAddInvoiceForm(false);
     setShowEditInvoiceForm(false);
     setShowDeleteModal(false);
-    setCurrentInvoice(null);
   };
 
   return (
@@ -24,8 +22,6 @@ export const InvoiceProvider = ({ children }) => {
         setShowEditInvoiceForm,
         showDeleteModal,
         setShowDeleteModal,
-        currentInvoice,
-        setCurrentInvoice,
         clearAll,
       }}
     >
