0b268565b4377fa425ef4f4255443cce2b6f4ac8
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-24 20:17:24 -0500

    Refactor component
    Gave Image layout responsive and moved into containing element
    Now resizes with screen size properly
    Changed message below image to show 'New' or 'New Invoice' depending on screen size

--- components/layout/Invoice404.jsx
+++ components/layout/Invoice404.jsx
@@ -2,20 +2,25 @@ import Image from 'next/image';
 
 export default function Invoice404() {
   return (
-    <div className='mt-24 md:mt-52 lg:mt-36 flex flex-col items-center'>
-      <Image
-        src='/assets/illustration-empty.svg'
-        alt='no invoices found'
-        width='193px'
-        height='160px'
-      />
-      <div className='mt-10'>
+    <div className='flex flex-col items-center gap-10 md:gap-16 mt-24 md:mt-52 lg:mt-36'>
+      <div className='w-48 h-40 md:w-64 md:h-52'>
+        <Image
+          src='/assets/illustration-empty.svg'
+          alt='no invoices found'
+          width='242px'
+          height='200px'
+          layout='responsive'
+        />
+      </div>
+      <div>
         <h2 className='text-xl font-bold tracking-tighter text-center'>
           There is nothing here
         </h2>
-        <p className='text-xs text-six tracking-tight text-center mt-5 mx-auto w-52'>
+        <p className='text-xs text-six tracking-tight text-center mt-5 w-56'>
           Create an invoice by clicking the{' '}
-          <span className='font-bold'>New</span> button and get started
+          <span className='font-bold md:hidden'>New</span>
+          <span className='font-bold hidden md:inline'>New Invoice</span> button
+          and get started
         </p>
       </div>
     </div>
