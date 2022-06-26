b071f0d6259b5d7ef4a0b603bcb33b9de927de5d
Ivan Olmo <ivan.olmo@gmail.com>
2022-06-25 21:34:33 -0500

    Change filter menu closing behavior
    Added functionality to close filter menu when any outside clicks occur

--- components/ui/FilterButton.jsx
+++ components/ui/FilterButton.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 export default function FilterButton({ setFilters }) {
-  // TODO add click outside
   const [checkedStatus, setCheckedStatus] = useState({
     draft: false,
     pending: false,
@@ -10,12 +9,39 @@ export default function FilterButton({ setFilters }) {
   });
   const [showFilters, setShowFilters] = useState(false);
 
+  const filterRef = useRef();
+  const buttonRef = useRef();
+
   const handleCheck = (status) => {
     const val = checkedStatus[status];
     const updateCheckedState = { ...checkedStatus, [status]: !val };
 
     setCheckedStatus(updateCheckedState);
   };
+
+  const handleToggle = () => {
+    setShowFilters(!showFilters);
+  };
+
+  // handles closing the filter menu if touch/click occurs outside menu
+  // adapted from https://stackoverflow.com/questions/32553158/detect-click-outside-react-component/42234988#42234988
+  useEffect(() => {
+    const clickOutside = (e) => {
+      if (
+        filterRef.current &&
+        !filterRef.current.contains(e.target) &&
+        !buttonRef.current.contains(e.target)
+      ) {
+        setShowFilters(false);
+      }
+    };
+
+    document.addEventListener('mousedown', clickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', clickOutside);
+    };
+  }, []);
 
   useEffect(() => {
     setFilters(
@@ -28,8 +54,9 @@ export default function FilterButton({ setFilters }) {
   return (
     <div className='relative'>
       <div
+        ref={buttonRef}
+        onClick={handleToggle}
         className='flex items-center gap-3 lg:gap-4 cursor-pointer dark:text-white'
-        onClick={() => setShowFilters(!showFilters)}
       >
         <span className='text-xs font-bold md:hidden'>Filter</span>
         <span className='text-xs lg:text-sm font-bold hidden md:block'>
@@ -43,7 +70,10 @@ export default function FilterButton({ setFilters }) {
         />
       </div>
       {showFilters && (
-        <div className='absolute top-10 -left-10 lg:-left-7 flex flex-col gap-4 bg-white dark:bg-three border border-none rounded-lg w-max p-6 md:w-48 shadow-[0_8px_10px_0px_rgba(124,93,250,0.3)] dark:shadow-[0_8px_10px_0px_rgba(20,22,37,0.9)]'>
+        <div
+          ref={filterRef}
+          className='absolute top-10 -left-10 lg:-left-7 flex flex-col gap-4 bg-white dark:bg-three border border-none rounded-lg w-max p-6 md:w-48 shadow-[0_8px_10px_0px_rgba(124,93,250,0.3)] dark:shadow-[0_8px_10px_0px_rgba(20,22,37,0.9)]'
+        >
           {Object.keys(checkedStatus).map((status, index) => (
             <div
               key={index}
