53ce129ddb268b18da41da12b18222bb77927383
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-09 18:15:25 -0500

    Update form validation schemas for sign in/sign up pages

--- utils/validationSchemas.js
+++ utils/validationSchemas.js
@@ -33,20 +33,31 @@ export const formValidationSchema = Yup.object({
     .min(1, 'At least one item is required'),
 });
 
-export const signUpValidationSchema = Yup.object({
-  name: Yup.string().required(),
-  email: Yup.string().email().required(),
+export const registerValidationSchema = Yup.object({
+  name: Yup.string().optional(),
+  email: Yup.string().email('Invalid Email').required('Required'),
   password: Yup.string()
     .password()
-    .required()
-    .min(6)
+    .required('Required')
+    .min(6, 'Minimum 6 characters')
     .max(20)
     .minNumbers(0)
+    .minLowercase(0)
+    .minUppercase(0)
+    .minSymbols(0),
+  confirmPassword: Yup.string()
+    .password()
+    .required('Required')
+    .oneOf([Yup.ref('password')], 'Passwords do not match')
+    .min(6, 'Minimum 6 characters')
+    .max(20)
+    .minNumbers(0)
+    .minLowercase(0)
     .minUppercase(0)
     .minSymbols(0),
 });
 
 export const signInValidationSchema = Yup.object({
-  email: Yup.string().email().required(),
-  password: Yup.string().required().min(6),
+  email: Yup.string().email('Invalid Email').required('Required'),
+  password: Yup.string().required('Required'),
 });
