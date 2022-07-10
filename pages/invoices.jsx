ac7b1ea430fe1c7f8a67ef8073056fcb9b011383
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-10 00:56:56 -0500

    Add new invoices page
    Was previously the index page
    Auth required to view this page
    getServerSideProps to be reimplemented later

---
+++ pages/invoices.jsx
@@ -0,0 +1,66 @@
+import { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { collection, orderBy, query } from 'firebase/firestore';
+
+import { useAuth } from '../context/AuthContext';
+import InvoiceContext from '../context/InvoiceContext';
+import InvoiceAdd from '../components/invoice/InvoiceAdd';
+import InvoiceList from '../components/invoice/InvoiceList';
+import { db } from '../lib/firebase';
+import { useCollectionData } from '../lib/hooks';
+
+export default function Home() {
+  const { user } = useAuth();
+
+  const { showAddInvoiceForm } = useContext(InvoiceContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/signin');
+    }
+  }, [user, router]);
+
+  const [data, loading, error] = useCollectionData(
+    user
+      ? query(
+          collection(db, 'users', user.uid, 'invoices'),
+          orderBy('paymentDue', 'asc')
+        )
+      : null
+  );
+
+  if (error) throw new Error();
+
+  return !user ? (
+    <></>
+  ) : (
+    <div className='grid grid-cols-1 md:justify-items-center w-full lg:h-screen lg:overflow-y-scroll'>
+      <InvoiceList invoices={data} loading={loading} />
+
+      {showAddInvoiceForm && <InvoiceAdd />}
+    </div>
+  );
+}
+
+// export async function getServerSideProps() {
+//   const snapshot = await getDocs(
+//     query(collection(db, 'invoices'), orderBy('paymentDue', 'asc'))
+//   );
+
+//   if (!snapshot.docs) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   const initialData = snapshot.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   }));
+
+//   return {
+//     props: { initialData },
+//   };
+// }
