bc78a5da1fb4fe49e360d433ee8d6ed17b016003
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-10 00:26:34 -0500

    Refactor header
    Several changes
    Pulls user from context and displays conditional image
    No user -> login icon (also a link to sign in)
    User with no image -> generic user icon (also a link to user profile)
    User with image (Google or Github) -> user image (also a link to user profile)
    Update main logo to match design
    Update light/dark theme toggle icons

--- components/layout/MainHeader.jsx
+++ components/layout/MainHeader.jsx
@@ -1,22 +1,25 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 
+import { useAuth } from '../../context/AuthContext';
 import InvoiceContext from '../../context/InvoiceContext';
+import LoginIcon from '../ui/LoginIcon';
+import UserIcon from '../ui/UserIcon';
 
 export default function MainHeader() {
-  const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
   const { clearAll } = useContext(InvoiceContext);
+  const { user } = useAuth();
 
-  useEffect(() => setMounted(true), []);
+  const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <header className='flex lg:flex-col justify-between items-center bg-slate-700 dark:bg-slate-900 lg:rounded-r-3xl lg:h-screen'>
-      <Link href='/'>
+      <Link href='/invoices'>
         <a onClick={() => clearAll()}>
-          <div className='bg-violet-500 p-6 md:p-7 lg:p-8 leading-zero rounded-r-3xl'>
+          <div className='bg-violet-500 p-6 md:p-7 lg:p-8 leading-zero rounded-r-3xl relative'>
+            <div className='absolute top-1/2 bottom-0 left-0 right-0 bg-violet-400 rounded-tl-3xl rounded-br-3xl'></div>
             <div className='w-7 h-7 lg:w-10 lg:h-10'>
               <Image
                 src='/assets/logo.svg'
@@ -31,36 +34,57 @@ export default function MainHeader() {
       </Link>
       <div className='flex lg:flex-col lg:gap-7 items-center leading-zero'>
         <div
-          className='mr-6 lg:m-0 cursor-pointer w-5 h-5'
+          className='mr-6 lg:m-0 cursor-pointer w-8 h-8 lg:w-10 lg:h-10'
           onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
         >
           {resolvedTheme === 'light' ? (
             <Image
-              src='/assets/icon-moon.svg'
+              src='/assets/dark-mode.svg'
               alt='theme switcher'
-              width='20px'
-              height='20px'
+              width='32px'
+              height='32px'
               layout='responsive'
             />
           ) : (
             <Image
-              src='/assets/icon-sun.svg'
+              src='/assets/light-mode.svg'
               alt='theme switcher'
-              width='20px'
-              height='20px'
+              width='32px'
+              height='32px'
               layout='responsive'
             />
           )}
         </div>
         <div className='px-6 py-5 md:py-6 lg:px-8 lg:py-6 border-l lg:border-l-0 lg:border-t-2 border-slate-600'>
-          <div className='rounded-full overflow-hidden w-8 h-8 lg:w-10 lg:h-10'>
-            <Image
-              src='/assets/image-avatar.jpg'
-              alt='user avatar'
-              width='32px'
-              height='32px'
-              layout='responsive'
-            />
+          <div
+            className='rounded-full overflow-hidden w-8 h-8 lg:w-10 lg:h-10'
+            id='login'
+          >
+            {!user ? (
+              <Link href='/signin'>
+                <a>
+                  <LoginIcon className='w-8 h-8 lg:w-10 lg:h-10 fill-current text-emerald-400' />
+                </a>
+              </Link>
+            ) : (
+              <Link href='/profile'>
+                <a>
+                  {user && !user.photoURL ? (
+                    <UserIcon className='w-8 h-8 lg:w-10 lg:h-10' />
+                  ) : (
+                    user && (
+                      <Image
+                        src={user?.photoURL}
+                        alt='user avatar'
+                        width='32px'
+                        height='32px'
+                        layout='responsive'
+                      />
+                    )
+                  )}
+                </a>
+              </Link>
+            )}
           </div>
         </div>
       </div>
