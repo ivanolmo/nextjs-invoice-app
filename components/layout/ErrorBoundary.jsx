692c5293caad15ee4cf4fbaf1837fbae461a805a
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-14 18:38:39 -0500

    Refactor onclick function to refresh page

--- components/layout/ErrorBoundary.jsx
+++ components/layout/ErrorBoundary.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
+import { withRouter } from 'next/router';
 
-export default class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
+export default withRouter(
+  class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
 
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.log({ error, errorInfo });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className='mt-40 lg:mt-80 lg:mx-auto'>
-          <div className='flex flex-col items-center justify-center w-full gap-6'>
-            <h2>Oops, something went wrong!</h2>
-            <button
-              type='button'
-              onClick={() => this.setState({ hasError: false })}
-              className='px-6 py-4 text-xl text-white rounded-md bg-violet-500'
-            >
-              Try again?
-            </button>
-          </div>
-        </div>
-      );
+      this.state = { hasError: false };
     }
 
-    return this.props.children;
+    static getDerivedStateFromError(error) {
+      return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+      console.log({ error, errorInfo });
+    }
+
+    render() {
+      if (this.state.hasError) {
+        return (
+          <div className='mt-40 lg:mt-80 lg:mx-auto'>
+            <div className='flex flex-col items-center justify-center w-full gap-6'>
+              <h2>Oops, something went wrong!</h2>
+              <button
+                type='button'
+                onClick={() => {
+                  this.setState({ hasError: false });
+                  this.props.router.replace(router.asPath);
+                }}
+                className='px-6 py-4 text-xl text-white rounded-md bg-violet-500'
+              >
+                Try again?
+              </button>
+            </div>
+          </div>
+        );
+      }
+
+      return this.props.children;
+    }
   }
-}
+);
