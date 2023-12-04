20001657b8fbadd0db8db44c4f3413c03bd70ace
Ivan Olmo <ivan.olmo@gmail.com>
2023-12-04 16:33:35 -0600

    Update home page styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,24 +24,19 @@ export default function Home() {
         <div className='space-y-4'>
           <h2 className='font-bold'>About this app</h2>
           <p>
-            This full stack web application is a{' '}
-            <a
-              href='https://www.frontendmentor.io/challenges/invoice-app-i7KaLTQjl'
-              className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'
-            >
-              challenge on Frontendmentor.io
-            </a>
-            . It allows a user to create, read, update, and delete invoices.
+            This full stack web application allows users to create, read,
+            update, and delete invoices. It&apos;s designed to be user-friendly
+            and efficient in managing invoices.
           </p>
           <p>
-            The layout and design system is provided with a Figma file from
-            Frontendmentor, and the goal is to create a fully functional app
-            using any tech stack.
+            The layout and design system is modern and intuitive, aiming to
+            provide a seamless user experience. The app is fully functional and
+            is built with a versatile tech stack to ensure high performance and
+            reliability.
           </p>
           <p>
-            As an additonal challenge, I decided to integrate Firebase for both
-            user authentication and the Firestore NoSQL database for invoice
-            storage.
+            As an advanced feature, the app integrates Firebase for both user
+            authentication and the Firestore NoSQL database for invoice storage.
           </p>
         </div>
         <div className='space-y-4'>
@@ -63,97 +58,114 @@ export default function Home() {
           <h2 className='font-bold'>Technologies used</h2>
           <ul className='space-y-4'>
             <li>
-              <a href='https://reactjs.org/'>
-                <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
-                  <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
-                    React
-                  </span>
-                  <Image
-                    src='/assets/icon-arrow-right.svg'
-                    alt='navigation arrow'
-                    width='7px'
-                    height='10px'
-                  />
-                </div>
-              </a>
+              <Link href='https://react.dev/'>
+                <a>
+                  <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
+                    <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
+                      React
+                    </span>
+                    <Image
+                      src='/assets/icon-arrow-right.svg'
+                      alt='navigation arrow'
+                      width='7px'
+                      height='10px'
+                    />
+                  </div>
+                </a>
+              </Link>
             </li>
             <li>
-              <a href='https://nextjs.org'>
-                <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
-                  <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
-                    Next.js
-                  </span>
-                  <Image
-                    src='/assets/icon-arrow-right.svg'
-                    alt='navigation arrow'
-                    width='7px'
-                    height='10px'
-                  />
-                </div>
-              </a>
+              <Link href='https://nextjs.org'>
+                <a>
+                  <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
+                    <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
+                      Next.js
+                    </span>
+                    <Image
+                      src='/assets/icon-arrow-right.svg'
+                      alt='navigation arrow'
+                      width='7px'
+                      height='10px'
+                    />
+                  </div>
+                </a>
+              </Link>
             </li>
             <li>
-              <a href='https://tailwindcss.com/'>
-                <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
-                  <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
-                    Tailwind CSS
-                  </span>
-                  <Image
-                    src='/assets/icon-arrow-right.svg'
-                    alt='navigation arrow'
-                    width='7px'
-                    height='10px'
-                  />
-                </div>
-              </a>
+              <Link href='https://tailwindcss.com/'>
+                <a>
+                  <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
+                    <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
+                      Tailwind CSS
+                    </span>
+                    <Image
+                      src='/assets/icon-arrow-right.svg'
+                      alt='navigation arrow'
+                      width='7px'
+                      height='10px'
+                    />
+                  </div>
+                </a>
+              </Link>
             </li>
             <li>
-              <a href='https://firebase.google.com/'>
-                <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
-                  <span className='text-blue-900 border-b border-blue-900 dark:text-blue-400 dark:border-blue-400'>
-                    Firebase (Auth, Firestore, and Functions)
-                  </span>
-                  <Image
-                    src='/assets/icon-arrow-right.svg'
-                    alt='navigation arrow'
-                    width='7px'
-                    height='10px'
-                  />
-                </div>
-              </a>
+              <Link href='https://firebase.google.com/'>
+                <a>
+                  <div className='flex justify-between items-center bg-gray-200 dark:bg-gray-800 px-4 py-2.5 border rounded-lg dark:border-0'>
+                    <p className='text-blue-900 dark:text-blue-400'>
+                      <span className='border-b border-blue-900 dark:border-blue-400'>
+                        Firebase (Auth, Firestore, and Functions)
+                      </span>
+                    </p>
+                    <Image
+                      src='/assets/icon-arrow-right.svg'
+                      alt='navigation arrow'
+                      width='7px'
+                      height='10px'
+                    />
+                  </div>
+                </a>
+              </Link>
             </li>
           </ul>
         </div>
-        {!user && (
-          <div className='space-y-4'>
-            <h2 className='font-bold'>Ready to get started?</h2>
-            <div className='flex flex-col gap-8'>
+        <div className='space-y-4'>
+          <h2 className='font-bold'>Ready to get started?</h2>
+          <div className='flex flex-col gap-8'>
+            <div className='space-y-4'>
               <p>
-                Register a new account or sign in to your existing account below
+                If you&apos;d like to explore the app without creating an
+                account, feel free to use our test account. The credentials are
+                as follows:
               </p>
-              <div>
-                <Link href='/signup'>
-                  <a>
-                    <div className='flex items-center justify-center gap-2 px-6 py-4 rounded-full md:gap-4 bg-violet-500 hover:bg-violet-400'>
-                      <span className='font-bold text-white'>Sign Up</span>
-                      <SignUpIcon className='w-6 h-6 fill-current text-emerald-400' />
-                    </div>
-                  </a>
-                </Link>
-              </div>
-              <div>
-                <Link href='/signin'>
-                  <a>
-                    <div className='flex items-center justify-center gap-2 px-6 py-4 rounded-full md:gap-4 bg-violet-500 hover:bg-violet-400'>
-                      <span className='font-bold text-white'>Sign In</span>
-                      <LoginIcon className='w-6 h-6 fill-current text-emerald-400' />
-                    </div>
-                  </a>
-                </Link>
-              </div>
+              <p>Email: test@test.com</p>
+              <p>Password: password</p>
+            </div>
+            <p>
+              Register a new account or sign in to your existing account below:
+            </p>
+            <div>
+              <Link href='/signup'>
+                <a>
+                  <div className='flex items-center justify-center gap-2 px-6 py-4 rounded-full md:gap-4 bg-violet-500 hover:bg-violet-400'>
+                    <span className='font-bold text-white'>Sign Up</span>
+                    <SignUpIcon className='w-6 h-6 fill-current text-emerald-400' />
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <div>
+              <Link href='/signin'>
+                <a>
+                  <div className='flex items-center justify-center gap-2 px-6 py-4 rounded-full md:gap-4 bg-violet-500 hover:bg-violet-400'>
+                    <span className='font-bold text-white'>Sign In</span>
+                    <LoginIcon className='w-6 h-6 fill-current text-emerald-400' />
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
